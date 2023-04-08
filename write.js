410109b9248a6b46b1d9cf6cba602c92858030af
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-04-08 22:58:55 +0200

    Adding new functionality

--- write.js
+++ write.js
@@ -1,10 +1,15 @@
-import { readdirSync } from 'fs';
+import { readdirSync, readFileSync } from 'fs';
 import weaviate from 'weaviate-ts-client';
 
 const client = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',
 });
+
+const toBase64 = (file) => {
+    const img = readFileSync(file);
+    return Buffer.from(img).toString('base64');
+};
 
 const imageFiles = readdirSync('./images');
 
