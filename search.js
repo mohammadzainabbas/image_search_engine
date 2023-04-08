3b094733a9c7aa0c66e9eccf3b6bdb637d422f02
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-04-08 23:05:12 +0200

    Adding missing sanitization

--- search.js
+++ search.js
@@ -1,6 +1,8 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
 import weaviate from 'weaviate-ts-client';
 
+const LIMIT = 5;
+
 const client = weaviate.client({
     scheme: 'http',
     host: 'localhost:8080',
@@ -11,6 +13,21 @@ const toBase64 = (file) => {
     return Buffer.from(img).toString('base64');
 };
 
-const imageFiles = readdirSync('./test');
+const testImageFiles = readdirSync('./test');
+
+const promises = testImageFiles.map(async (imageFile) => {
+    const test_b64 = toBase64(`./test/${imageFile}`);
+
+    const resImage = await client.graphql.get()
+    .withClassName('Person')
+    .withFields(['image'])
+    .withNearImage({ image: test_b64 })
+    .withLimit(5)
+
+
+
+
+}
+
 
 
