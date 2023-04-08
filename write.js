adcd8470cf70ac2acbf18b7f1a659a7c5f0aaa0b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-04-08 22:57:52 +0200

    Code cleanup

--- write.js
+++ write.js
@@ -1,8 +1,16 @@
 import { readdirSync } from 'fs';
+import weaviate from 'weaviate-ts-client';
+
+const client = weaviate.client({
+    scheme: 'http',
+    host: 'localhost:8080',
+});
 
 const imageFiles = readdirSync('./images');
 
 const promises = imageFiles.map(async (imageFile) => {
-    
+    const b64 = toBase64(`./images/${imageFile}`);
+
+    await client
 
 console.log(imageFiles);
