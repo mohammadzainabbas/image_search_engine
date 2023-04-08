3b1beaaa1b8d3e326f04a8fa21f446f632b80a0a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-04-08 22:29:58 +0200

    Adding missing documentation

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ import weaviate from 'weaviate-ts-client';
 
 const client = new weaviate.Client({
     scheme: 'http',
-    host: 'localhost',
-    port: 8080,
-    pathPrefix: '/v1'
+    host: 'localhost:8080',
 });
